74596ff39a670b52462144a92418382df96e8f3a
LauValenciaD <lauravalencia26@gmail.com>
2025-03-08 19:40:43 +0100

    no ob

--- cerrar_sesion.php
+++ cerrar_sesion.php
@@ -1,6 +1,5 @@
 <!-- Hecho por Laura Valencia Díaz -->
 <?php
-ob_start();  // Inicia el buffer de salida
 
 session_start();  // Inicia la sesión
 session_unset();  // Elimina todas las variables de sesión
@@ -9,5 +8,5 @@ session_destroy();  // Destruye la sesión
 header("Location: index.php");  // Redirige al inicio de sesión
 exit();  // Asegúrate de que el script se detenga aquí
 
-ob_end_flush();  // Finaliza el buffer de salida
+
 ?>
